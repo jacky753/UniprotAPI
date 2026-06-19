a9aeb503241e491abc912de6c896b86e86dc15b6
ysimazaki <40457818+jacky753@users.noreply.github.com>
2026-06-20 00:31:28 +0900

    erase hard coding
    Updated database connection parameters in con_db function.

--- 01_UniprotAPI_XML_parser03_positivedata_domains_test.py
+++ 01_UniprotAPI_XML_parser03_positivedata_domains_test.py
@@ -312,42 +312,19 @@ def main():
 
 
     print("main END.")
-"""
+    
 def con_db():
     # コネクションの作成
     conn = mydb.connect(
         host='localhost',
         port=3306,
-        user='root',
-        password='miyazakilab',
-        #password='KtHk23#8', 
-        #database='meropsrefs01'
-        database='meropsweb12_1'
-        #database='meropsweb121'
-    )
-
-    # DB操作用にカーソルを作成
-    cur = conn.cursor()
-    return cur
-"""
-def con_db():
-    # コネクションの作成
-    conn = mydb.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        #password='miyazakilab',
-        password='KtHk23#8', 
-        #database='meropsrefs01'
-        #database='meropsweb12_1'
+        user='foo',
+        password='bar',
         database='meropsweb121'
     )
-
     # DB操作用にカーソルを作成
     cur = conn.cursor()
     return cur
-
-
 
 def safe_read_response(response, retries=3, delay=2):
     """IncompleteReadが出てもリトライして読み取る"""
